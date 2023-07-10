9374228aea347746c3cfcfaf383bcd0c1aac24fc
amirb <amir.bullock9@gmail.com>
2023-07-10 19:24:37 -0400

    add functionality to check column for a clicked cell, and add a clicked cell above it.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,30 @@
-const cells = document.querySelectorAll('.cell');
-let currentPlayer = 'red';
+document.addEventListener('DOMContentLoaded', function() {
+  const cells = document.querySelectorAll('.cell');
+  const columns = Array.from(Array(7).keys()); // Adjust the number of columns if needed
+  let currentPlayer = 'red';
 
-// Function to handle cell click event
-function handleClick(event) {
-  const cell = event.target.closest('.cell');
-  if (!cell) return;
-  
-  console.log('Cell clicked:', cell);
-  cell.classList.add(currentPlayer);
-  currentPlayer = (currentPlayer === 'red') ? 'blue' : 'red';
-}
+  // Function to handle cell click event
+  function handleClick(event) {
+    const clickedCell = event.target.closest('.cell');
+    if (!clickedCell) return;
 
-// Attach click event listener to cells
-cells.forEach((cell) => {
-  cell.addEventListener('click', handleClick);
+    const columnIndex = Array.from(clickedCell.parentNode.children).indexOf(clickedCell) % columns.length;
+
+    const columnCells = Array.from(cells).filter((_, index) => index % columns.length === columnIndex);
+
+    for (let i = columnCells.length - 1; i >= 0; i--) {
+      const cell = columnCells[i];
+      if (!cell.classList.contains('red') && !cell.classList.contains('blue')) {
+        cell.classList.add(currentPlayer);
+        break;
+      }
+    }
+
+    currentPlayer = (currentPlayer === 'red') ? 'blue' : 'red';
+  }
+
+  // Attach click event listener to cells
+  cells.forEach((cell) => {
+    cell.addEventListener('click', handleClick);
+  });
 });
